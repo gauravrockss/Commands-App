bd5bb8590cf5c1618909c115da9e4bb9c96ca86d
gauravrockss <gauravmaharishi6392@gmail.com>
2024-04-01 22:47:00 +0545

    list of command

--- src/main/java/com/projects/katat/Main.java
+++ src/main/java/com/projects/katat/Main.java
@@ -1,8 +1,15 @@
 package com.projects.katat;
 
+
 import java.util.Scanner;
+import java.util.Stack;
+
 
 public class Main {
+    private static final String COMMANDS_FILE_PATH = "src/main/java/com/projects/katat/commands/commands.json";
+    private static final Stack<String> commandHistory = new Stack<>();
+    private static final Stack<String> redoHistory = new Stack<>();
+    private static String[] commands;
     public static void main(String[] args) {
         // loadCommands();
         Scanner scanner = new Scanner(System.in);
@@ -17,7 +24,7 @@ public class Main {
                     // issueCommand();
                     break;
                 case "l":
-                    // listCommands();
+                    listCommands();
                     break;
                 case "u":
                     // undoCommand();
@@ -45,6 +52,13 @@ public class Main {
         System.out.println("r - Redo last undone command");
         System.out.println("q - Quit the application");
         System.out.print("Enter your command: ");
+    }
+
+    private static void listCommands() {
+        System.out.println("\nList of Available Commands:");
+        for (int i = 0; i < commands.length; i++) {
+            System.out.println((i + 1) + ". " + commands[i]);
+        }
     }
 
 }
